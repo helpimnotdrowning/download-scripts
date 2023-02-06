89c6c63ec2efcf3f725e81ed2b1764bfe959fb66
helpimnotdrowning <35247379+helpimnotdrowning@users.noreply.github.com>
2023-02-06 08:50:03 -0600

    Something has changed, I forgot what but it probably works a bit better now.

--- _subs-chat.bat
+++ _subs-chat.bat
@@ -1,3 +1,3 @@
 :: download the subtitles and chat of a video
 
-yt-dlp --format "bestvideo[vcodec^=avc]+bestaudio[acodec^=mp4a]" --verbose --skip-download --force-ipv4 --ignore-errors --no-continue --no-overwrites --cookies-from-browser firefox --all-subs --throttled-rate 100K --retries infinite --concurrent-fragments 4 --no-part --output "%%(uploader)s/%%(upload_date)s - %%(title)s/%%(title)s [%%(id)s] f%%(format_id)s.%%(ext)s" -- %* 2>&1
+yt-dlp --format "best" --verbose --skip-download --force-ipv4 --ignore-errors --no-continue --no-overwrites --cookies-from-browser firefox --all-subs --throttled-rate 100K --retries infinite --concurrent-fragments 4 --no-part --output "%%(uploader)s/%%(upload_date)s - %%(title)s/%%(title)s [%%(id)s] f%%(format_id)s.%%(ext)s" -- %* 2>&1
--- stream_unprocessed.bat
+++ stream_unprocessed.bat
@@ -1,3 +1,3 @@
 @ECHO off
 
-ytarchive  --trace --cookies cookies.txt --add-metadata --no-merge --write-description --write-thumbnail --threads 4 --output "%%(channel)s/%%(channel)s - %%(upload_date)s - %%(title)s/%%(channel)s - %%(upload_date)s - %%(title)s [%%(id)s]" %* best
+ytarchive  --trace --cookies cookies.txt --add-metadata --no-merge --write-description --write-thumbnail --retry-stream 600 --threads 4 --output "%%(channel)s/%%(channel)s - %%(upload_date)s - %%(title)s/%%(channel)s - %%(upload_date)s - %%(title)s [%%(id)s]" %* best
